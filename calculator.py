494b6f8380c0897db29ecd04dcc7d0fa27cf8ddb
colidur <durand.coline.dc@gmail.com>
2019-02-03 23:30:58 +0100

    Updated with Except

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
-
-#calculatrice
+#Pythnon3
+#Calculator
 
 import re
 
@@ -12,23 +12,47 @@ run = True
 def performMath():
     global run
     global previous
-    equation=""
+    equation = ""
+
 
     if previous == 0:
         equation = input("Enter equation: ")
+        
     else:
-        equation = input("Enter equation: "+str(previous))
+        print("\n---- Type 'quit' to exit -------")
+        print("---- Type 'restart' to clear ---\n")
+        equation = input("Enter equation: "+ str(previous) + " ")
+        
+        
+    try:
+        
+        if equation == 'quit':
+            print("You've decided to stop the program.")
+            run = False
+            
+        elif equation == 'restart':
+            print("You've decided to restart the program.")
+            previous = 0
     
-    if equation == 'quit':
-        print("You've decided to stop the program.")
-        run = False
-    else:
-        equation = re.sub('[a-zA-Z:" ","\'"]','',equation)
-        if previous == 0:
-            previous = eval(equation)
         else:
-            previous = eval(str(previous) + equation)
-        print(previous)
+            equation = re.sub('[a-zA-Z:" ","\'"]', '', equation)
+            
+            if previous == 0:
+                previous = eval(equation)
+                
+            else:
+                previous = eval(str(previous) + equation)
+            print(previous)
+    
+    
+    except ZeroDivisionError:
+        print("/!\ Please do not divide by zero")
+    
+    except SyntaxError:
+        print("/!\ Please use numbers or mathematics symbols only")
+
+    except NameError:
+        print("/!\ Please use numbers")
 
 while run:
     performMath()
